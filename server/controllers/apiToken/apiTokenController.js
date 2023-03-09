380cafe708efe3b26b5e9b2c04d9a737133c0811
hsm00 <106731623+hsm00@users.noreply.github.com>
2023-03-09 09:08:53 +0100

    rollback

--- server/controllers/apiToken/apiTokenController.js
+++ server/controllers/apiToken/apiTokenController.js
@@ -83,8 +83,8 @@ exports.openAI = async (req, res) => {
 
     The structure of the list should be like this: artist - song.\n\n`;
 
-    const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
         prompt: prompt,
         max_tokens: 120,
         temperature: 0.9,
