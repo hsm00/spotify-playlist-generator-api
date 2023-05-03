1760154cdf44ae78ad1ceae10f92ddaa1d356cee
hsm00 <106731623+hsm00@users.noreply.github.com>
2023-05-03 20:34:03 +0200

    Update apiToken.js

--- server/routes/apiToken/apiToken.js
+++ server/routes/apiToken/apiToken.js
@@ -7,19 +7,6 @@ const router = express.Router();
 
 app.use(cors());
 
-const whitelist = ["https://aiplaylist.netlify.app"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-router.options('/api', cors(corsOptions));
-
 router.post('/login', apiToken.getAccessToken);
 router.post('/refresh', apiToken.getRefreshToken);
 router.post('/api', apiToken.openAI)
